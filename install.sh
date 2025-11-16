3f83549a0cfea890c3c6d1b59394f8f60614240e
primeZdev <the.primez.dev@gmail.com>
2025-11-16 12:45:15 +0330

    improve: Installing dependencies

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ set -e
 
 APP_NAME="ov-node"
 INSTALL_DIR="/opt/$APP_NAME"
+VENV_DIR="/opt/${APP_NAME}_venv"
 REPO_URL="https://github.com/primeZdev/ov-node"
 PYTHON="/usr/bin/python3"
 
@@ -10,11 +11,20 @@ GREEN="\033[0;32m"
 YELLOW="\033[1;33m"
 NC="\033[0m"
 
+echo -e "${YELLOW}Updating system...${NC}"
 apt update -y
-apt install -y python3 python3-pip wget curl git -y
-pip3 install --upgrade pip
-pip3 install colorama pexpect requests uuid
+apt install -y python3 python3-venv python3-full wget curl git
 
+python3 -m venv "$VENV_DIR"
+
+source "$VENV_DIR/bin/activate"
+
+pip install --upgrade pip setuptools wheel
+
+echo -e "${YELLOW}Installing dependencies in venv...${NC}"
+pip install colorama pexpect requests uuid
+
+# Download repo release
 if [ ! -d "$INSTALL_DIR" ]; then
     echo -e "${YELLOW}Downloading latest release...${NC}"
 
@@ -30,10 +40,12 @@ if [ ! -d "$INSTALL_DIR" ]; then
     echo -e "${YELLOW}Extracting...${NC}"
     tar -xzf latest.tar.gz -C "$INSTALL_DIR" --strip-components=1
     rm -f latest.tar.gz
-
 else
     echo -e "${GREEN}Directory exists, skipping download.${NC}"
 fi
 
+echo -e "${YELLOW}Running installer...${NC}"
 cd "$INSTALL_DIR"
-$PYTHON installer.py
+$VENV_DIR/bin/python installer.py
+
+echo -e "${GREEN}Installation completed successfully!${NC}"
